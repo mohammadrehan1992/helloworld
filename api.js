f65c47303f7b281bc4be3534c13b9cd3833e4456
mohammadrehan1992 <r.mohammadrehan@gmail.com>
2025-07-31 16:52:29 +0500

    Add GET API endpoint for user with firstname and last

---
+++ api.js
@@ -0,0 +1,20 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  if (req.method === 'GET' && req.url === '/api/user') {
+    const user = {
+      firstname: 'John',
+      last: 'Doe'
+    };
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    res.end(JSON.stringify(user));
+  } else {
+    res.writeHead(404, { 'Content-Type': 'text/plain' });
+    res.end('Not Found');
+  }
+});
+
+const PORT = 3000;
+server.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}/`);
+});
